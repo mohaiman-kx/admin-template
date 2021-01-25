931f6f74d819a529fc18bb44ec3fd406221c29b9
mohaiman-kx <moh.kxtech@gmail.com>
2021-01-25 20:02:23 +0600

    product pages added skleton

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,3 +3,12 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 });
+
+
+$(document).ready(function () {
+    $('#editSeo').click(function () {
+        console.log('hello')
+        $('.no-seo-area').toggle();
+        $('.seo-content').toggle();
+    })
+})
