a7d7f9b8924eed380668a497252e5137a3478030
mohaiman-kx <moh.kxtech@gmail.com>
2021-01-31 13:06:39 +0600

    another commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,3 +12,19 @@ $(document).ready(function () {
         $('.seo-content').toggle();
     })
 })
+
+
+
+function addVarient() {
+    var vClone = document.getElementById('v-items').cloneNode(true);
+    document.getElementById('form-container').appendChild(vClone);
+}
+
+$(document).ready(function () {
+    $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
